480c1737a0d02f991c95d4201cce10be339aeff2
Danil Markov <markov.danil.04@mail.ru>
2022-10-21 20:07:55 +0300

    тут немного решения, но я чуть позже доделаю

---
+++ 6-Колекции(Массивы и объекты)/lesson.js
@@ -0,0 +1,14 @@
+const purchase = {
+    Electonics:[
+        {name: 'Laptop',price:1500},
+        {name: 'Keyboard',price:100},
+        {name: 'HDMI cable',price:10},
+    ],
+    Textile:[
+        {name: 'Bag', price:50},
+    ],
+};
+
+
+const key = Object.keys(purchase);
+console.log(key); 
