e296fbbbd0fbbff0683d851989966e50d1863d56
Danil Markov <markov.danil.04@mail.ru>
2022-10-21 22:49:08 +0300

    Продолжаем

--- 6-Колекции(Массивы и объекты)/lesson.js
+++ 6-Колекции(Массивы и объекты)/lesson.js
@@ -31,3 +31,21 @@ console.log([...items,
         //Добавление элемена в массив
 purchase.Electonics.push({name:'Mouse',price:25});
 console.log(purchase.Electonics.at(-1));
+
+const item = purchase.Electonics.pop(); //изъятие ластового элемента
+console.log(item);
+console.log(purchase.Electonics);
+
+
+const prices = [10,1500,100,50];
+const price = prices.find((item)=>item>100);
+console.log({price});
+
+
+const names =['laptop','keyboard','mouse'];
+console.log(names.join('; '));
+const index = names.indexOf('mouse');
+console.log({index});
+
+const test = purchase.Electonics.filter((item)=>item.price>50);
+console.log({test});
