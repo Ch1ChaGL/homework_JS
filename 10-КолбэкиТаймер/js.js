db51be8ae3f519cbc23fa88252bba82a5896c2a9
Danil Markov <markov.danil.04@mail.ru>
2022-10-25 21:03:38 +0300

    Разобрался, но надо повторить функции немного, синтакисис и их объявления и отличия и все такое

--- 10-КолбэкиТаймер/js.js
+++ 10-КолбэкиТаймер/js.js
@@ -9,14 +9,32 @@
 // console.log(res);
 // });
 
-// const add = (a,b,callback) =>{
-//     if(!Number.isFinite(a)|| !Number.isFinite(b)){
-//         callback(new Error('Invalid arguments'));
-//         return;
-//     }
-//     callback(null,a+b);
+const add = (a,b,callback) =>{
+    if(!Number.isFinite(a)|| !Number.isFinite(b)){
+        callback(new Error('Invalid arguments'));
+        return;
+    }
+    callback(0,a+b);
+};
+// callback last error first
+
+
+/*КОроче спустя полтора часа наверное, я осознал что происходит, вопервых колбэк 
+это термин а не какаято общая функция, можно назвать свою колбэк функцию как угодно, и вывести тоже как угодно*/
+// function SayHellow () {
+// console.log('im callback function');
 // };
-//callback last error first
+// const sum = (a,b, HellowCallback)=>{
+//     console.log(a+b);
+//     HellowCallback();
+// }
+// sum(2,4,SayHellow);
+/*А в самом задание, мы вызываем функцию add в нее передаем два параметра и функцию, потом срабатывает функция add
+и мы сравниваем на число, если нет то мы вызываем функцию колбэк с одним значением, а так как при передаче неполных значений 
+в функциЮ, такие значения становятся undefind, потому результат и undefind, если же они оба числа, то функция колбэк вызывается
+с перым значением 0/null/undefind/false, как флаг, того что ошибки нет, и if в колбэк функции не сработает ,и выведет просто значение,
+почему передается стрелочная функция а разбита на несколько частей, трудно сказать,но наверное так надо */
+
 // const res = add(1,3,(error,result)=>{
 // if(error) console.error(error);
 // console.log({result});
@@ -35,27 +53,34 @@
 // console.log(avialable);
 
 
-const printA = (callback)=>{
-    console.log('A');
-    callback();
-};
-const printB = (callback) => {
-    console.log('B');
-    callback();
-};
-const printC = (callback) => {
-    console.log('C');
-    callback();
-}
+// const printA = (callback)=>{
+//     console.log('A');
+//     callback();
+// };
+// const printB = (callback) => {
+//     console.log('B');
+//     callback();
+// };
+// const printC = (callback) => {
+//     console.log('C');
+//     callback();
+// }
 
-printA(()=>{
-    printB(()=>{
-        printA(()=>{
-            printC(()=>{
-                printB(()=>{
-                    console.log("END");
-                })
-            });
-        });
-    });
-});
+// printA(()=>{
+//     printB(()=>{
+//         printA(()=>{
+//             printC(()=>{
+//                 printB(()=>{
+//                     console.log("END");
+//                 })
+//             });
+//         });
+//     });
+// });
+
+
+const arguments = -1;
+
+if(arguments){
+    console.log("hellow");
+}
