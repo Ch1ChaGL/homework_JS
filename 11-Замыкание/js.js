78f25d0dbcf7c6ebaaa10e35c64be19191c3d002
Danil Markov <markov.danil.04@mail.ru>
2022-10-27 22:22:42 +0300

    Небольшие примеры использования замыканий

--- 11-Замыкание/js.js
+++ 11-Замыкание/js.js
@@ -16,5 +16,47 @@ const add = (x)=> (y)=>{
     console.log(`${x}+${y}=${z}`);
     return z;
 }
-const result = add(3)(5);
-console.log(result);
+const result = add(3);
+console.log(result(5));
+
+
+const logger = (kind) =>{
+    const colors = {
+        warning : '\x1b[1;33m',
+        error: '\x1b[0;31m',
+        info: '\x1b[1;37m'
+    };
+    const color = colors[kind] || colors.info;
+    return(s)=>{
+        const date = new Date().toISOString();
+        console.log(color+date+'\t'+s);
+    };
+};
+
+const warning = logger('warning');
+const error = logger('error');
+const slow = logger('slow');
+
+slow('I am slow logger');
+warning('helloe');
+error('World');
+
+
+const generateKey = (alphabet) =>{
+    const max = alphabet.length;
+    return (length)=>{
+        let key = '';
+        for(let i = 0; i<length;i++){
+            const index = Math.floor(Math.random() * max);
+            key+=alphabet[index];
+        }
+        return key;
+    };
+};
+
+
+const chars = 'abcdefghiklmnopqrstuvwxyz';
+const generatePassword = generateKey(chars);
+
+const password = generatePassword(12);
+console.log({password});
