fefa8d3f74678ed00f459bcfd4acaadddc87f31c
Danil Markov <markov.danil.04@mail.ru>
2022-11-19 22:50:47 +0300

    123

---
+++ 18-Введение1/2js.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const MAX_VALUE = 10;
+
+console.log('begin');
+for (let i = 0; i < MAX_VALUE; i++) {
+  console.dir({ i, date: new Date() });
+}
+console.log('the end');
+
+
+const add = (a, b) => a + b;
+console.log('Add numbers : 5+2 = ' + add(5, 2));
+console.log('concatenate : \'5\'+\'2\' = ' + add('5', '2'));
+console.log("subtraction : 5 - 2 = " + add(5, -2));
+123
