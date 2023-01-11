7ec031471968125ca5138b9e49f6b2e99f98662c
Danil Markov <markov.danil.04@mail.ru>
2023-01-11 19:13:40 +0300

    bind, apply, call, selectionSort

--- GrokkingAlgorithms/selectionSort.js
+++ GrokkingAlgorithms/selectionSort.js
@@ -1,3 +1,27 @@
 'use strict';
 
 
+function findBigest(arr) {
+  let bigest = arr[0];
+  let bigestIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > bigest) {
+      bigest = arr[i];
+      bigestIndex = i;
+    }
+  }
+  return bigestIndex;
+}
+
+function seletionSort(arr) {
+  const newArr = [];
+  while (arr.length) {
+    const smallest = findBigest(arr);
+    newArr.push(arr[smallest]);
+    arr.splice(smallest, 1);
+  }
+  return newArr;
+}
+
+
+console.log(seletionSort([5, 3, 6, 2, 10]));
