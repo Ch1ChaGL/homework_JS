90ab265c0b6dc0c518fb8d4b555e8b01931f6721
Danil Markov <markov.danil.04@mail.ru>
2023-02-07 21:17:36 +0300

    Proxy Reflect

--- learnjs/Proxy и Reflect/Proxy1.js
+++ learnjs/Proxy и Reflect/Proxy1.js
@@ -96,7 +96,12 @@ for (const key in  user) {
 
 
 
-let user2 = {};
+let user2 = {
+  a: 1,
+  b: 1,
+  c: 1,
+  d: 1,
+};
 
 user2 = new Proxy(user2, {
   ownKeys(target) {
@@ -112,3 +117,128 @@ user2 = new Proxy(user2, {
 
 
 console.log(Object.keys(user2));
+
+
+
+let userTest = {
+  name: 'Danil',
+  _secret: '1234',
+};
+
+
+
+userTest = new Proxy(userTest, {
+  get(target, property) {
+    if (property.startsWith('_')) throw new Error('Отказано в доступе');
+    else {
+      const value = target[property];
+      return (typeof value === 'function') ? value.bind(target) : value;
+    }
+  },
+  set(target, property, value) {
+    if (property.startsWith('_')) throw new Error('Отказано в доступе');
+    target[property] = value;
+    return true;
+  },
+  deleteProperty(target, property) {
+    if (property.startsWith('_')) throw new Error('Отказано в доступе');
+    delete target[property];
+    return true;
+  },
+  ownKeys(target) {
+    return Object.keys(target).filter(key => !key.startsWith('_'));
+  }
+});
+
+console.log('-----------------');
+// "get" не позволяет прочитать _password
+try {
+  console.log(userTest._password); // Error: Отказано в доступе
+} catch (e) { console.log(e.message); }
+
+console.log('----------------');
+// "set" не позволяет записать _password
+try {
+  userTest._password = 'test'; // Error: Отказано в доступе
+} catch (e) { console.log(e.message); }
+
+// "deleteProperty" не позволяет удалить _password
+try {
+  delete userTest._password; // Error: Отказано в доступе
+} catch (e) { console.log(e.message); }
+
+// "ownKeys" исключает _password из списка видимых для итерации свойств
+for (const key in userTest) console.log(key); // name
+
+
+let range = {
+  start: 1,
+  end: 10,
+};
+
+range = new Proxy(range, {
+  has(target, property) {
+    return property >= target.start && property <= target.end;
+  }
+});
+
+console.log(5 in range);
+console.log(50 in range);
+
+
+// function delay(f, ms) {
+//   // возвращает обёртку, которая вызывает функцию f через таймаут
+//   return function() { // (*)
+//     setTimeout(() => f.apply(this, arguments)``, ms);
+//   };
+// }
+
+// function sayHi(user) {
+//   console.log(`Привет, ${user}!`);
+// }
+
+// // после обёртки вызовы sayHi будут срабатывать с задержкой в 3 секунды
+// sayHi = delay(sayHi, 3000);
+
+// sayHi('Вася');
+
+function delay(f, ms) {
+  return new Proxy(f, {
+    apply(target, thisArg, argsArray) {
+      setTimeout(() => target.apply(thisArg, argsArray), ms);
+    }
+  });
+}
+
+function sayHi(user) {
+  console.log(`Привет, ${user}!`);
+}
+
+
+sayHi = delay(sayHi, 3000);
+
+
+sayHi('Данил');
+console.log(sayHi.length);
+
+
+
+{
+  let user = {
+    name: 'Danil',
+  };
+
+  user = new Proxy(user, {
+    get(target, property, receiver) {
+      console.log('Get');
+      return Reflect.get(target, property, receiver);
+    },
+    set(target, property, value, receiver) {
+      console.log('Set');
+      return Reflect.set(target, property, value, receiver);
+    },
+  });
+
+  const name = user.name;
+  user.name = 'Danil Markov';
+}
