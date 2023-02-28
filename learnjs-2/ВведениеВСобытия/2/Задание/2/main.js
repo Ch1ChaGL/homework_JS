c7d9bae767c8e37a68ae8fccf541cdd215c58917
Danil Markov <markov.danil.04@mail.ru>
2023-02-28 19:35:39 +0300

    Изучил парочку тем, поповторял предыдущие, в целом нормально, но можно лучше, меня беспокоит что если не повторять материал то из-за того, что какие-то темы я очень редко использую, они просто забудуться, вот промисы я помню, а вот прокси тоже конечно помню, но не так сильно, а генераторы так вообще помню что есть, а как и что делают, не особо помню, а верстку я уже очень давно не трогал, так что хз как оно пойдет, но хочется сделать какой-то мини проект, а лучше были бы лабы по js, я бы тогда с увеличением сложности практиковал проекты

---
+++ learnjs-2/ВведениеВСобытия/2/Задание/2/main.js
@@ -0,0 +1,15 @@
+'use strict';
+const tree = document.querySelector('.tree');
+
+for (const li of tree.querySelectorAll('li')) {
+  const span = document.createElement('span');
+  li.prepend(span);
+  span.append(span.nextSibling);
+}
+
+tree.addEventListener('click', event => {
+  if (event.target.tagName !== 'SPAN') return;
+  const childContainer = event.target.parentNode.querySelector('ul');
+  if (!childContainer) return;
+  childContainer.hidden = !childContainer.hidden;
+});
