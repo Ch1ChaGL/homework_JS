ebe353f6fbc5f99ee5e076c39e8616af7d2b7589
Danil Markov <markov.danil.04@mail.ru>
2023-03-06 12:37:07 +0300

    Повторил , разобрал что-то, координаты сложная тема на самом деле

---
+++ learnjs-2/ИнтерфейсныеСобытия/1/main.js
@@ -0,0 +1,62 @@
+'use strict';
+
+
+/*mousedown mouseup кнопка мыши нажата или отпущена
+  mouseover mouseout курсор появляется на элементе или уходит с него
+  mousemove при движении мыши
+  click
+  dbclick
+  contextmenu
+*/
+
+
+/*
+    События с кликом имеют свойство event.button
+    оно позволяет отследить какой кнопкой вызвано событие
+
+    Состояние кнопки	event.button
+    Левая кнопка (основная)	0
+    Средняя кнопка (вспомогательная)	1
+    Правая кнопка (вторичная)	2
+    Кнопка X1 (назад)	3
+    Кнопка X2 (вперёд)	4
+*/
+
+/*
+    Модификаторы: shift, alt, ctrl и meta
+    Все события мыши включают в себя
+    информацию о нажатых клавишах-модификаторах.
+
+    Свойства события:
+
+    shiftKey: Shift
+    altKey: Alt (или Opt для Mac)
+    ctrlKey: Ctrl
+    metaKey: Cmd для Mac
+    Они равны true, если во время события была нажата соответствующая клавиша
+*/
+
+
+const btn1 = document.querySelector('.btn1');
+
+function event1(event) {
+  if (event.shiftKey && event.altKey) {
+    alert('Событие сработало');
+  }
+}
+
+
+btn1.addEventListener('click', event1);
+
+
+const input1 = document.querySelector('.input1');
+const test = document.querySelector('.test');
+
+
+input1.addEventListener('mousemove', event => {
+  const box = test.getBoundingClientRect();
+  const x = event.clientX - box.left;
+  const y = event.clientY - box.top;
+  event.currentTarget.value = `${x} : ${y}`;
+});
+
