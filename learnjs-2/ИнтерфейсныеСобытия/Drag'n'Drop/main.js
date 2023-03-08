e2acacb48a67e65318baab74ffce0cab61a45a9f
Danil Markov <markov.danil.04@mail.ru>
2023-03-08 21:15:01 +0300

    Drag'n'Drop

---
+++ learnjs-2/ИнтерфейсныеСобытия/Drag'n'Drop/main.js
@@ -0,0 +1,64 @@
+'use strict';
+// Базовый алгоритм Drag’n’Drop выглядит так:
+
+// При mousedown – готовим элемент к перемещению,
+//если необходимо (например, создаём его копию).
+// Затем при mousemove передвигаем элемент на
+// новые координаты путём смены left/top и position:absolute.
+// При mouseup – остановить перенос элемента
+//и произвести все действия, связанные с окончанием Drag’n’Drop.
+
+
+const ball = document.querySelector('.ball');
+const container = document.querySelector('.container');
+ball.ondragstart = function() {
+  return false;
+};
+ball.onmousedown  =  event => {
+
+
+  const containerCoords = container.getBoundingClientRect();
+
+
+  const shiftX = event.clientX - ball.offsetLeft;
+  const shiftY = event.clientY - ball.offsetTop;
+
+  ball.style.position = 'absolute';
+  ball.style.zIndex = 1000;
+
+  container.append(ball);
+
+  function moveAt(pageX, pageY) {
+
+
+    const left = pageX - shiftX;
+    const top = pageY -  shiftY;
+
+    if (left >= containerCoords.left &&
+        left + ball.offsetWidth <=
+        containerCoords.left + container.offsetWidth &&
+        top >= containerCoords.top &&
+        top + ball.offsetHeight <=
+        containerCoords.top + container.offsetHeight) {
+      ball.style.left = left + 'px';
+      ball.style.top = top + 'px';
+    } else {
+      container.removeEventListener('mousemove', onMoseMove);
+      ball.onmouseup = null;
+    }
+  }
+  moveAt(event.pageX, event.pageY);
+
+
+  function onMoseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  container.addEventListener('mousemove', onMoseMove);
+
+  ball.onmouseup = () => {
+    container.removeEventListener('mousemove', onMoseMove);
+    ball.onmouseup = null;
+  };
+
+};
