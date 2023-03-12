b4b23f3f16e9850b2ce4e270a1d30479777b8512
Danil Markov <markov.danil.04@mail.ru>
2023-03-12 21:36:25 +0300

    Проработали полосу прокрутки и немного события указателя, было сложновато, будто геометрию учим

--- learnjs-2/ИнтерфейсныеСобытия/Drag'n'Drop/main.js
+++ learnjs-2/ИнтерфейсныеСобытия/Drag'n'Drop/main.js
@@ -62,3 +62,63 @@ ball.onmousedown  =  event => {
   };
 
 };
+
+
+
+
+
+
+
+
+
+const thumb = document.querySelector('.thumb');
+const slider = document.querySelector('.slider');
+const word = document.querySelector('.word');
+
+word.addEventListener('slider', event => {
+  word.style.fontSize = event.detail.fontSize + 'px';
+});
+
+thumb.addEventListener('mousedown', event => {
+  event.preventDefault();
+  const shiftX = event.clientX - thumb.getBoundingClientRect().left;
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  function onMouseMove(event) {
+    let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+
+    const rightEdge = slider.offsetWidth - thumb.offsetWidth;
+
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+    thumb.style.left = newLeft + 'px';
+
+    const k = newLeft / rightEdge;
+    const fz = 14 + (k * 10);
+
+    const newEvent = new CustomEvent('slider', {
+      detail: {
+        fontSize: fz,
+      }
+    });
+    word.dispatchEvent(newEvent);
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+});
+
+thumb.ondragstart = function() {
+  return false;
+};
+
+
